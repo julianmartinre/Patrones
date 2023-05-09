37f246369aac69a717d7d14de7d82f99ca237889
DESKTOP-0IG0NTD\Julián <julian.martin.93@gmail.com>
2023-05-09 09:41:45 -0300

    Adapter.

---
+++ Patrones.Adapter/Motor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patrones.Adapter
+{
+    public abstract class Motor
+    {
+        public abstract void Acelerar();
+        public abstract void Arrancar();
+        public abstract void Detener();
+        public abstract void CargarCombustible();
+    }
+}
---
+++ Patrones.Adapter/MotorNaftero.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patrones.Adapter
+{
+    public class MotorNaftero : Motor
+    {
+        public override void Acelerar()
+        {
+            Console.WriteLine("Acelerando el motor naftero..");
+        }
+
+        public override void Arrancar()
+        {
+            Console.WriteLine("Arrancando el motor naftero..");
+        }
+
+        public override void CargarCombustible()
+        {
+            Console.WriteLine("Cargando combustible al motor naftero..");
+        }
+
+        public override void Detener()
+        {
+            Console.WriteLine("Deteniendo el motor naftero..");
+        }
+    }
+}
